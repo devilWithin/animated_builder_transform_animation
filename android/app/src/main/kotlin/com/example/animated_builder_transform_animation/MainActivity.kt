080396cfe98bf693cff323d2805a819bb9192f08
Mostafa Medhat <mostafamedhat544@gmail.com>
2023-02-27 19:36:11 +0200

    First commit with all the code

---
+++ android/app/src/main/kotlin/com/example/animated_builder_transform_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animated_builder_transform_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AnimatedBuilderTransformAnimation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 3,
+      ),
+    );
+    _animation = Tween<double>(begin: 0.0,end: math.pi * 2).animate(_controller);
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) => Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()..rotateZ(_animation.value),
+            child: Card(
+              elevation: 5,
+              shadowColor: Colors.black54,
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.teal, borderRadius: BorderRadius.circular(8)),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
